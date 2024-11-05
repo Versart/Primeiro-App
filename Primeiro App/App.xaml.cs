4b2ecdea0cb3aa983b4560a909897ad748487a38
Versart <wesleyversart@gmail.com>
2024-11-04 23:17:08 -0300

    Adicionar arquivos de projeto.

---
+++ Primeiro App/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Primeiro_App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Primeiro App/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Primeiro_App
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
